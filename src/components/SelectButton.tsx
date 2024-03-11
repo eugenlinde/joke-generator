a811e361bda8c9691eadbed6311d02e3cd6792b3
Eugen L <eugen.linde95@gmail.com>
2024-03-11 18:41:42 +0200

    refactor select

---
+++ src/components/SelectButton.tsx
@@ -0,0 +1,15 @@
+import { Listbox } from "@headlessui/react";
+import { ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+const SelectButton = ({ value }: { value: string }) => (
+  <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+    <span className="flex items-center">
+      <span className="ml-3 block truncate">{value}</span>
+    </span>
+    <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+      <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+    </span>
+  </Listbox.Button>
+);
+
+export default SelectButton;
---
+++ src/components/SelectOption.tsx
@@ -0,0 +1,43 @@
+import { Listbox } from "@headlessui/react";
+import { CheckIcon } from "@heroicons/react/20/solid";
+import { classNames } from "../util/generalUtil";
+
+const SelectOption = ({ value }: { value: string }) => (
+  <Listbox.Option
+    key={value}
+    className={({ active }) =>
+      classNames(
+        active ? "bg-indigo-600 text-white" : "text-gray-900",
+        "relative cursor-default select-none py-2 pl-3 pr-9"
+      )
+    }
+    value={value}
+  >
+    {({ selected, active }) => (
+      <>
+        <div className="flex items-center">
+          <span
+            className={classNames(
+              selected ? "font-semibold" : "font-normal",
+              "ml-3 block truncate"
+            )}
+          >
+            {value}
+          </span>
+        </div>
+        {selected ? (
+          <span
+            className={classNames(
+              active ? "text-white" : "text-indigo-600",
+              "absolute inset-y-0 right-0 flex items-center pr-4"
+            )}
+          >
+            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+          </span>
+        ) : null}
+      </>
+    )}
+  </Listbox.Option>
+);
+
+export default SelectOption;
